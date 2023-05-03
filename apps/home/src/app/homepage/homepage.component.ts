406d33ea3d31394b4cce71392ea4777db529d722
Silviu Tanasa <silviu.tanasa@raro.eu>
2023-05-03 17:07:40 +0300

    feature#5: create shared lib for api calls

--- apps/home/src/app/homepage/homepage.component.ts
+++ apps/home/src/app/homepage/homepage.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { ApiCallsService } from '@courses-app/shared/api'
 
 @Component({
-  selector: 'courses-app-homepage',
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.sass'],
+    selector: 'courses-app-homepage',
+    templateUrl: './homepage.component.html',
+    styleUrls: ['./homepage.component.sass'],
 })
-export class HomepageComponent {}
+export class HomepageComponent {
+    testProp = 'Initial test prop'
+
+    constructor(private apiCalls: ApiCallsService) {
+    }
+
+
+    // this is just to illustrate the call from the shared lib, can be deleted
+    callApi() {
+        this.apiCalls.getTest().subscribe(res => {
+            console.log('res', res)
+            this.testProp = 'api called';
+        })
+    }
+}
