2fab2a3a68135e4c12444704bdbb2d7445e23016
Silviu Tanasa <silviu.tanasa@raro.eu>
2023-05-08 09:44:50 +0300

    Feature#9: add angular material to project

--- apps/home/src/app/homepage/homepage.module.ts
+++ apps/home/src/app/homepage/homepage.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage.component';
 import {Route, RouterModule} from "@angular/router";
+import {MatButtonModule} from "@angular/material/button";
 
 const homeRoutes: Route[] = [
   {path: '', component: HomepageComponent}
@@ -9,6 +10,6 @@ const homeRoutes: Route[] = [
 
 @NgModule({
   declarations: [HomepageComponent],
-  imports: [CommonModule, RouterModule.forChild(homeRoutes)],
+  imports: [CommonModule, RouterModule.forChild(homeRoutes), MatButtonModule],
 })
 export class HomepageModule {}
